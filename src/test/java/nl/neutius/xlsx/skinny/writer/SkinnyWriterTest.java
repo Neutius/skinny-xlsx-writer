97dddb39514272a5ed41c7927dc7e778f16ea00b
Neutius <sfcgijs@gmail.com>
2020-06-28 12:46:13 +0200

    Basic cleanup and using JUnit5 @TempDir annotation

--- src/test/java/nl/neutius/xlsx/skinny/writer/SkinnyWriterTest.java
+++ src/test/java/nl/neutius/xlsx/skinny/writer/SkinnyWriterTest.java
@@ -3,13 +3,11 @@ package nl.neutius.xlsx.skinny.writer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,94 +16,8 @@ import java.util.List;
 
 class SkinnyWriterTest extends AbstractSkinnyWriterTestBase {
 
-    protected File targetFolder;
-
-    @BeforeEach
-    void setUpTargetFolder() {
-        targetFolder = new File("D:\\dev\\test\\excel");
-        File[] fileArray = targetFolder.listFiles();
-
-        if (fileArray != null && fileArray.length > 0) {
-            for (File file : fileArray) {
-                assertThat(file.delete()).isTrue();
-            }
-        }
-    }
-
     @Test
-    void verifySetUp_targetFolderIsUsable() {
-        assertThat(targetFolder).exists();
-        assertThat(targetFolder).isDirectory();
-        assertThat(targetFolder).isEmptyDirectory();
-        assertThat(targetFolder).canRead();
-        assertThat(targetFolder).canWrite();
-    }
-
-    @Test
-    void createNewFile_fileExists() throws IOException {
-        writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
-
-        writer.createNewXlsxFile();
-
-        File expectedFile = new File(targetFolder, FILE_NAME + EXTENSION);
-        assertThat(expectedFile).exists();
-    }
-
-    @Test
-    void createNewFile_emptyFileIsValidXlsxFile() throws IOException, InvalidFormatException {
-        writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
-
-        writer.createNewXlsxFile();
-
-        File targetFile = new File(targetFolder, FILE_NAME + EXTENSION);
-        XSSFWorkbook actualWorkbook = new XSSFWorkbook(targetFile);
-        XSSFSheet actualSheet = actualWorkbook.getSheetAt(0);
-
-        assertThat(actualSheet).isNotNull();
-        assertThat(actualSheet).isEmpty();
-        assertThat(actualSheet).hasSize(0);
-        assertThat(actualSheet.getActiveCell()).isNull();
-        assertThat(actualSheet.getFirstRowNum()).isEqualTo(-1);
-        assertThat(actualSheet.getLastRowNum()).isEqualTo(-1);
-        assertThat(actualSheet.getCellComments()).isEmpty();
-        assertThat(actualSheet.getSheetName()).isEqualTo(SHEET_NAME);
-        assertThat(actualSheet).isEqualTo(actualWorkbook.getSheet(SHEET_NAME));
-    }
-
-    @Test
-    void addContent_fileHasContent() throws IOException, InvalidFormatException {
-        writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
-
-        writer.createNewXlsxFile();
-        writer.addRowToCurrentSheet(List.of("entry"));
-        writer.writeToFile();
-        XSSFWorkbook actualWorkbook = new XSSFWorkbook(new File(targetFolder, FILE_NAME + EXTENSION));
-
-        Sheet actualSheet = actualWorkbook.getSheet(SHEET_NAME);
-        assertThat(actualSheet).isNotEmpty();
-        assertThat(actualSheet).hasSize(1);
-        assertThat(actualSheet.getFirstRowNum()).isEqualTo(0);
-        assertThat(actualSheet.getLastRowNum()).isEqualTo(0);
-
-        Row actualRow = actualSheet.getRow(0);
-        assertThat(actualRow).isNotNull();
-        assertThat(actualRow).isNotEmpty();
-        assertThat(actualRow).hasSize(1);
-        assertThat((int) actualRow.getFirstCellNum()).isEqualTo(0);
-        assertThat((int) actualRow.getLastCellNum()).isEqualTo(1);
-        assertThat(actualRow.getPhysicalNumberOfCells()).isEqualTo(1);
-        assertThat(actualRow.getRowNum()).isEqualTo(0);
-
-        Cell actualCell = actualRow.getCell(0);
-        assertThat(actualCell).isNotNull();
-        assertThat(actualCell.getSheet()).isEqualTo(actualSheet);
-        assertThat(actualCell.getStringCellValue()).isEqualTo("entry");
-        assertThat(actualCell.getColumnIndex()).isEqualTo(0);
-        assertThat(actualCell.getRowIndex()).isEqualTo(0);
-    }
-
-    @Test
-    void addSeveralRows_allRowsArePresent() throws IOException, InvalidFormatException {
+    void addSeveralRows_allRowsArePresent(@TempDir File targetFolder) throws IOException, InvalidFormatException {
         writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
 
         writer.createNewXlsxFile();
@@ -126,7 +38,7 @@ class SkinnyWriterTest extends AbstractSkinnyWriterTestBase {
     }
 
     @Test
-    void addSeveralRowsAsOneList_allRowsArePresent() throws IOException, InvalidFormatException {
+    void addSeveralRowsAsOneList_allRowsArePresent(@TempDir File targetFolder) throws IOException, InvalidFormatException {
         writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
 
         writer.createNewXlsxFile();
@@ -145,7 +57,7 @@ class SkinnyWriterTest extends AbstractSkinnyWriterTestBase {
     }
 
     @Test
-    void addSeveralRowsAndColumns_allFieldsArePresent() throws IOException, InvalidFormatException {
+    void addSeveralRowsAndColumns_allFieldsArePresent(@TempDir File targetFolder) throws IOException, InvalidFormatException {
         writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
 
         List<String> firstRow = List.of("entry0", "1", "?", "Mariënberg", "Curaçao");
@@ -183,7 +95,7 @@ class SkinnyWriterTest extends AbstractSkinnyWriterTestBase {
     }
 
     @Test
-    void addValuesWithWhiteSpaceCharacters_contentHasSameWhiteSpaceCharacters() throws IOException, InvalidFormatException {
+    void addValuesWithWhiteSpaceCharacters_contentHasSameWhiteSpaceCharacters(@TempDir File targetFolder) throws IOException, InvalidFormatException {
         writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
         String valueWithTabs = "we \t like \t tabs \t\t\t too \t much";
         String valueWithNewLines = "we \n use \n new \n lines \n\n\n\n within a \n single \n value";
@@ -207,7 +119,7 @@ class SkinnyWriterTest extends AbstractSkinnyWriterTestBase {
     }
 
     @Test
-    void addRowWithNullValues_areConvertedToEmptyStrings() throws IOException, InvalidFormatException {
+    void addRowWithNullValues_areConvertedToEmptyStrings(@TempDir File targetFolder) throws IOException, InvalidFormatException {
         writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
         List<String> entryList = new ArrayList<>();
         entryList.add("value");
@@ -235,7 +147,7 @@ class SkinnyWriterTest extends AbstractSkinnyWriterTestBase {
     }
 
     @Test
-    void valuesWithDifferentLengthAndHeight_columnWidthIsAdjusted() throws IOException, InvalidFormatException {
+    void valuesWithDifferentLengthAndHeight_columnWidthIsAdjusted(@TempDir File targetFolder) throws IOException, InvalidFormatException {
         writer = new SkinnyWriter(targetFolder, FILE_NAME, SHEET_NAME);
         String longValue = "we like writing an entire book in a single cell within a bigger spreadsheet, " +
                 "also, we \t like \t tabs \t\t\t too \t much";
