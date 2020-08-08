55511ddaf6ad09a41fd07ab0a592a5a9f93aca4f
Neutius <sfcgijs@gmail.com>
2020-08-08 15:33:05 +0200

    Added JavaDoc comments to recently added interface and default implementation

--- src/main/java/com/github/neutius/skinny/xlsx/writer/DefaultSheetContent.java
+++ src/main/java/com/github/neutius/skinny/xlsx/writer/DefaultSheetContent.java
@@ -2,6 +2,13 @@ package com.github.neutius.skinny.xlsx.writer;
 
 import java.util.List;
 
+/**
+ * A default implementation for the <code>SkinnySheetContent</code> interface - see the JavaDoc for that interface for more
+ * information.
+ * <p>
+ * This class has a private constructor and two static factory methods.
+ */
+
 public final class DefaultSheetContent implements SkinnySheetContent {
 
     private final String sheetName;
@@ -9,9 +16,32 @@ public final class DefaultSheetContent implements SkinnySheetContent {
     private final List<String> columnHeaders;
     private final List<List<String>> contentRows;
 
+    /**
+     * This method creates and returns a representation of a sheet to be added to a .xlsx file by the SkinnyWriter class,
+     * with a single column header row at the top of the sheet.
+     *
+     * @param sheetName     The name of the sheet to be added.
+     * @param columnHeaders Represents the column header row: A List of String values to be added to the sheet as column headers.
+     * @param contentRows   Represents the content rows: zero or more Lists
+     *                      containing zero or more String values to be added as content cell values.
+     * @return A representation of a sheet to be added to a .xlsx file by the SkinnyWriter class.
+     * Implements the SkinnySheetContent interface, allowing this return value to be passed directly to the SkinnyWriter class.
+     */
+
     public static DefaultSheetContent withHeaders(String sheetName, List<String> columnHeaders, List<List<String>> contentRows) {
         return new DefaultSheetContent(sheetName, true, columnHeaders, contentRows);
     }
+
+    /**
+     * This method creates and returns a representation of a sheet to be added to a .xlsx file by the SkinnyWriter class,
+     * with no column header row.
+     *
+     * @param sheetName     The name of the sheet to be added.
+     * @param contentRows   Represents the content rows: zero or more Lists
+     *                      containing zero or more String values to be added as content cell values.
+     * @return A representation of a sheet to be added to a .xlsx file by the SkinnyWriter class.
+     * Implements the SkinnySheetContent interface, allowing this return value to be passed directly to the SkinnyWriter class.
+     */
 
     public static DefaultSheetContent withoutHeaders(String sheetName, List<List<String>> contentRows) {
         return new DefaultSheetContent(sheetName, false, null, contentRows);
--- src/main/java/com/github/neutius/skinny/xlsx/writer/SkinnySheetContent.java
+++ src/main/java/com/github/neutius/skinny/xlsx/writer/SkinnySheetContent.java
@@ -2,13 +2,50 @@ package com.github.neutius.skinny.xlsx.writer;
 
 import java.util.List;
 
+/**
+ * This interface represents a single sheet to be added to a .xlsx file by the SkinnyWriter class.
+ * <p>
+ * Important note: For any instance of any implementation of this interface, if <code>hasColumnHeaders()</code> returns true, then
+ * <code>getColumnHeaders()</code> has to return a List with at least one String, containing at least one non-whitespace character.
+ */
+
 public interface SkinnySheetContent {
+
+    /**
+     * This method should return the name of the sheet to be added to the .xlsx file.
+     *
+     * @return The name of the sheet to be added. See <code>SkinnyWriter.addSheetToWorkbook(String)</code> for handling of edge
+     * cases.
+     */
 
     String getSheetName();
 
+    /**
+     * This method informs the SkinnyWriter class whether or not column headers should be added.
+     *
+     * @return A boolean value, with "true" meaning "column headers should be added to the sheet", and "false" meaning the opposite.
+     */
+
     boolean hasColumnHeaders();
 
+    /**
+     * This method should return all values for the column headers, if applicable. Each value should be a non-blank String value.
+     *
+     * @return A List of String values to be added, in order, as column headers.
+     * If <code>hasColumnHeaders()</code> returns true, this method is not allowed to return null, an empty List, or a List
+     * containing anything but String values. Each String value should contain at least one non-whitespace character.
+     * If, and only if, <code>hasColumnHeaders()</code> returns false, this method will not be called,
+     * and no valid return value is required.
+     */
+
     List<String> getColumnHeaders();
+
+    /**
+     * This method should return all content values to be added to the sheet.
+     *
+     * @return The content values to be added to the sheet.
+     * Any combination of null values, empty Lists, and blank Strings is allowed.
+     */
 
     List<List<String>> getContentRows();
 
